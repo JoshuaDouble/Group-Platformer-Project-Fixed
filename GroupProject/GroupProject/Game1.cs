c690a99209479ecb44e037cfc021e708ac947cc9
Joshua Double <joshua.double@ad.aie.edu>
2016-07-06 20:10:11 +0930

    The new improved visual studios platformer game

---
+++ GroupProject/GroupProject/Game1.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Maps.Tiled;
+using MonoGame.Extended.ViewportAdapters;using System;
+
+namespace GroupProject
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Player player = null;
+
+        Camera2D camera = null;
+        TiledMap map = null;
+        TiledTileLayer collisionLayer;
+        TiledTileLayer spikes;
+        TiledTileLayer Collums;
+        TiledTileLayer Ladder;
+
+        public static int tile = 64;
+        // abitrary choice for 1m (1 tile = 1 meter)
+        public static float meter = tile;
+        // very exaggerated gravity (6x)
+        public static float gravity = meter * 9.8f * 6.0f;
+        // max vertical speed (10 tiles/sec horizontal, 15 tiles/sec vertical)
+        public static Vector2 maxVelocity = new Vector2(meter * 10, meter * 15);
+        // horizontal acceleration - take 1/2 second to reach max velocity
+        public static float acceleration = maxVelocity.X * 2;
+        // horizontal friction - take 1/6 second to stop from max velocity
+        public static float friction = maxVelocity.X * 6;
+        // (a large) instantaneous jump impulse
+        public static float jumpImpulse = meter * 1500;
+
+
+        public int ScreenWidth
+        {
+            get
+            {
+                return graphics.GraphicsDevice.Viewport.Width;
+            }
+        }
+        public int ScreenHeight
+        {
+            get
+            {
+                return graphics.GraphicsDevice.Viewport.Height;
+            }
+        }
+
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            player = new Player(this);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            player.Load(Content);
+
+            var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice,
+            ScreenWidth, ScreenHeight);
+            camera = new Camera2D(viewportAdapter);
+            camera.Position = new Vector2(0, ScreenHeight);
+
+            map = Content.Load<TiledMap>("level1");
+            foreach (TiledTileLayer layer in map.TileLayers)
+            {
+                if (layer.Name == "Collisions")
+                    collisionLayer = layer;
+            }
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            player.Update(deltaTime);
+            camera.Position = player.Position - new Vector2(ScreenWidth / 2, ScreenHeight / 2);
+
+            base.Update(gameTime);
+        }
+
+      
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            var transformMatrix = camera.GetViewMatrix();
+
+            spriteBatch.Begin(transformMatrix: transformMatrix);
+
+            map.Draw(spriteBatch);
+            spriteBatch.End();
+
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+        public int PixelToTile(float pixelCoord)
+        {
+            return (int)Math.Floor(pixelCoord / tile);
+        }
+        public int TileToPixel(int tileCoord)
+        {
+            return tile * tileCoord;
+        }
+        public int CellAtPixelCoord(Vector2 pixelCoords)
+        {
+            if (pixelCoords.X < 0 ||
+           pixelCoords.X > map.WidthInPixels || pixelCoords.Y < 0)
+                return 1;
+            // let the player drop of the bottom of the screen (this means death)
+            if (pixelCoords.Y > map.HeightInPixels)
+                return 0;
+            return CellAtTileCoord(
+           PixelToTile(pixelCoords.X), PixelToTile(pixelCoords.Y));
+        }
+        public int CellAtTileCoord(int tx, int ty)
+        {
+            if (tx < 0 || tx >= map.Width || ty < 0)
+                return 1;
+            // let the player drop of the bottom of the screen (this means death)
+            if (ty >= map.Height)
+                return 0;
+            TiledTile tile = collisionLayer.GetTile(tx, ty);
+            return tile.Id;
+        }
+    }
+}
---
+++ GroupProject/GroupProject/Sprite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+namespace GroupProject
+{
+    class Sprite
+    {
+        public Vector2 position = Vector2.Zero;
+        public Vector2 offset = Vector2.Zero;
+        Texture2D texture;
+        List<AnimatedTexture> animations = new List<AnimatedTexture>();
+        List<Vector2> animationOffsets = new List<Vector2>();
+        int currentAnimation = 0;
+        public Sprite()
+        {
+        }
+        public void Load(ContentManager content, string asset)
+        {
+            texture = content.Load<Texture2D>(asset);
+        }
+        public void Add(AnimatedTexture animation, int xOffset = 0, int yOffset = 0)
+        {
+            animations.Add(animation);
+            animationOffsets.Add(new Vector2(xOffset, yOffset));
+        }
+        public void Update(float deltaTime)
+        {
+            animations[currentAnimation].UpdateFrame(deltaTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            animations[currentAnimation].DrawFrame(spriteBatch,
+            position + animationOffsets[currentAnimation]);
+        }
+    }
+}
+
