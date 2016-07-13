8c188eda37caf4229de7f8ae4eac29b01d428ddd
Alexander Nielsen-titus <alexander.niels@ad.aie.edu>
2016-07-13 18:11:29 +0930

    started goal movement of sprite

--- GroupProject/GroupProject/Game1.cs
+++ GroupProject/GroupProject/Game1.cs
@@ -213,6 +213,6 @@ namespace GroupProject
             TiledTile tile = collisionLayer.GetTile(tx, ty);
             return tile.Id;
         }
-      
+       
     }
 }
---
+++ GroupProject/GroupProject/goal.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Maps.Tiled;
+using MonoGame.Extended.ViewportAdapters;using System;using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace GroupProject
+{
+    class goal
+    {
+        Sprite sprite = new Sprite();
+
+        public void Load(ContentManager content)
+        {
+            AnimatedTexture animation = new AnimatedTexture(new Vector2(0, 0), 0, 0.2f, 1);
+            animation.Load(content, "goal", 6, 5);
+            sprite.Add(animation, 0, -25);            sprite.Pause();
+        }
+    }
+}
